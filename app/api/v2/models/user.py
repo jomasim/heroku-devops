fcbd8670eae54a26f2c3aa02a0a8fe48ea3ca061
Joma Simeon <simjoms@gmail.com>
2018-10-24 23:41:11 +0300

    [ft #161453434] refactored user model

--- app/api/v2/models/user.py
+++ app/api/v2/models/user.py
@@ -1,26 +1,19 @@
 from app.api.v2.database import DBConnection
 from werkzeug.security import generate_password_hash
+from psycopg2 import sql
 
 cur = DBConnection.get_connection().cursor()
 
 
 class User(object):
-    def __init__(self, name, email, username, password):
-
-        self.id = "",
-        self.name = name,
-        self.email = email,
-        self.username = username,
-        self.password = password
-        self.role = None
-
+   
     ''' create user '''
-
-    def create(self):
-        query = "INSERT INTO " \
-            "users (name,username,email,password,role)" \
-            "VALUES('%s','%s','%s','%s','%s')" % (
-                self.name, self.username, self.email, self.password, self.role)
-        cur.execute(query)
-        cur.commit()
-    
+    @staticmethod
+    def create_user(data):
+        query = sql.SQL("insert into {} ({}) values ({})").format(
+            sql.Identifier("users"),
+            sql.SQL(', ').join(map(sql.Identifier, data.keys())),
+            sql.SQL(', ').join(sql.Placeholder() * len(data))
+        )
+        cur.execute(query, data.values())
+     
