caac5ef86a89beb54ca607c5c9ac578635dc2606
Joma Simeon <simjoms@gmail.com>
2018-10-24 18:39:27 +0300

    [ch #161443801] added database schema

---
+++ app/schema/createdb.sql
@@ -0,0 +1,21 @@
+create table if not exists users(
+    id serial not null primary key,
+    name varchar not null,
+    username varchar not null,
+    email varchar not null,
+    role varchar not null,
+    password varchar not null,
+    create_at timestamp not null default now(),
+    updated_at timestamp default current_timestamp
+);
+
+create table if not exists products(
+    id serial not null primary key,
+    name varchar not null,
+    category varchar not null,
+    price varchar not null,
+    description text not null,
+    created_by integer not null references users(id),
+    create_at timestamp not null default now(),
+    updated_at timestamp default current_timestamp
+);
