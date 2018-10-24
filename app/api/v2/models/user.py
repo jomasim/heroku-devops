c0dba077ba78fda153dcb9bb48c720ba96e0dfcc
Joma Simeon <simjoms@gmail.com>
2018-10-24 20:10:28 +0300

    [ft #161453434] added user model

---
+++ app/api/v2/models/user.py
@@ -0,0 +1,26 @@
+from app.api.v2.database import DBConnection
+from werkzeug.security import generate_password_hash
+
+cur = DBConnection.get_connection().cursor()
+
+
+class User(object):
+    def __init__(self, name, email, username, password):
+
+        self.id = "",
+        self.name = name,
+        self.email = email,
+        self.username = username,
+        self.password = password
+        self.role = None
+
+    ''' create user '''
+
+    def create_user(self):
+        query = "INSERT INTO " \
+            "users (name,username,email,password,role)" \
+            "VALUES('%s','%s','%s','%s','%s')" % (
+                self.name, self.username, self.email, self.password, self.role)
+        cur.execute(query)
+        cur.commit()
+    
