593d20a94045a0b6651cfdab59e57a4226544ca8
Joma Simeon <simjoms@gmail.com>
2018-10-25 00:30:15 +0300

    [ft #161453434] added user exists method to user model

--- app/api/v2/models/user.py
+++ app/api/v2/models/user.py
@@ -6,7 +6,7 @@ cur = DBConnection.get_connection().cursor()
 
 
 class User(object):
-   
+
     ''' create user '''
     @staticmethod
     def create(data):
@@ -16,4 +16,15 @@ class User(object):
             sql.SQL(', ').join(sql.Placeholder() * len(data))
         )
         cur.execute(query, list(data.values()))
-     
+
+    @staticmethod
+    def exists(data):
+        if data['email']:
+            query = sql.SQL("select * from {} where {}= %s ").format(
+                sql.Identifier("users"),
+                sql.Identifier('email'),
+                data['email'])
+
+            if cur.execute(query):
+                return True
+        return False
