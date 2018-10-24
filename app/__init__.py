5f52a1a872abc351b391d9f13deb93a38b49ebcd
Joma Simeon <simjoms@gmail.com>
2018-10-24 20:37:46 +0300

    [ft #161453434] added user endpoint to api

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 from utils import env
 from instance.api_config import api_config
+from app.api.v2.views.user_resource import UserController
 
 api_blueprint = Blueprint("store-api", __name__, url_prefix='/api/v2')
 jwt = JWTManager()
@@ -19,6 +20,8 @@ def create_app(config_setting):
 
     ''' setting api blueprint  '''
     api = Api(api_blueprint)
+    api.add_resource(UserController, '/user/',
+                     strict_slashes=False, endpoint='post_user')
 
     app.register_blueprint(api_blueprint)
 
