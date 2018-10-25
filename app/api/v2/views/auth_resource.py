618a9180201e3ac48013341aa3a477365b75a165
Joma Simeon <simjoms@gmail.com>
2018-10-25 03:01:29 +0300

    [ft #161467475] added auth controller view

--- app/api/v2/views/auth_resource.py
+++ app/api/v2/views/auth_resource.py
@@ -21,10 +21,10 @@ class AuthController(Resource):
             password = data['password']
 
             if User.exists({'email':email}):
-
+                user=User.get_by_email(email)
                 if check_password_hash(user['password'], password):
                     exp = datetime.timedelta(minutes=20)
-                    token = create_access_token(user['username'], exp)
+                    token = create_access_token(user['email'], exp)
                     return make_response(jsonify({"message": "login successful",
                                                   "access_token": token}), 200)
             return make_response(jsonify({"message": "invalid login"}), 401)
