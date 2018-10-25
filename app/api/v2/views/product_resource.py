326be48cc03cfcf90b2c5d8dd64113727d194663
Joma Simeon <simjoms@gmail.com>
2018-10-25 09:31:31 +0300

    [ft #161468072] updated post product

--- app/api/v2/views/product_resource.py
+++ app/api/v2/views/product_resource.py
@@ -1,6 +1,8 @@
 from flask import jsonify, make_response, request
 from flask_restful import Resource
 from app.api.v2.request import Request
+from app.api.v2.models.product import Product
+
 
 class ProductController(Resource):
     def get(self, product_id=None):
@@ -19,22 +21,24 @@ class ProductController(Resource):
 
     def post(self):
         data = request.get_json()
-        request_schema = {'name': 'required',
-                          'category': 'required',
-                          'description':'required',
-                          'price':'required',
-                          'quantity':'required'
-                          }
-        validator=Request(data, request_schema)
-        if validator.validate() == None :
-            if not data:
-                return make_response(jsonify({'error': 'invalid data'}), 422)
-            product_id = len(products)+1
-            product = {'id': product_id, 'name': data['name'], 'category': data['category'],
-                    'description': data['description'],
-                    'price': data['price'], 'quantity': data['quantity']
-                    }
-            products.append(product)
-            return make_response(jsonify({'products': products}), 201)
+        ''' if productid exists in request delete product '''
+        if data['product_id']:
+            if Product.delete_by_Id(data['product_id']):
+                return make_response(jsonify({'message': "product deleted successfully"}), 201)
+            return make_response(jsonify({'message': "product does not exist"}), 404)
         else:
-            return make_response(jsonify(validator.validate()), 422)
+            request_schema = {'name': 'required',
+                            'category': 'required',
+                            'description': 'required',
+                            'price': 'required',
+                            }
+
+            validator = Request(data, request_schema)
+            if validator.validate() == None:
+
+                ''' create product '''
+                Product.create(data)
+
+                return make_response(jsonify({'message': "product created successfully"}), 201)
+            else:
+                return make_response(jsonify(validator.validate()), 422)
