9bfb86d7fb7021358a9aa1c07a2d82c915e91206
Joma Simeon <simjoms@gmail.com>
2018-10-25 03:04:21 +0300

    [ft # 161467475] added auth to api endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import JWTManager
 from utils import env
 from instance.api_config import api_config
 from app.api.v2.views.user_resource import UserController
+from app.api.v2.views.auth_resource import AuthController
 
 api_blueprint = Blueprint("store-api", __name__, url_prefix='/api/v2')
 jwt = JWTManager()
@@ -22,6 +23,8 @@ def create_app(config_setting):
     api = Api(api_blueprint)
     api.add_resource(UserController, '/user/',
                      strict_slashes=False, endpoint='post_user')
+    api.add_resource(AuthController, '/auth/',
+                     strict_slashes=False, endpoint='auth')
 
     app.register_blueprint(api_blueprint)
 
