e4cff8e0f30d945fe146219897a572fe05ec312e
Joma Simeon <simjoms@gmail.com>
2018-10-25 09:35:43 +0300

    [ft #161468072] added get product by id

--- app/api/v2/models/product.py
+++ app/api/v2/models/product.py
@@ -2,7 +2,9 @@ from app.api.v2.database import DBConnection
 import psycopg2.extras
 
 
-cur = DBConnection.get_connection().cursor(cursor_factory = psycopg2.extras.DictCursor)
+cur = DBConnection.get_connection().cursor(
+    cursor_factory=psycopg2.extras.DictCursor)
+
 
 class Product(object):
 
@@ -12,20 +14,24 @@ class Product(object):
 
         query = "INSERT INTO products (name,category,description,price,user)" \
                 "VALUES('%s','%s', '%s', '%s', '%s')" % (
-                    data['name'],data['category'],data['price'],data['description'],data['create_by'])
+                    data['name'], data['category'], data['price'], data['description'], data['create_by'])
         cur.execute(query)
-        
+
     @staticmethod
     def delete_by_Id(product_id):
         if product_id:
-            query="SELECT * FROM products WHERE id = '%s';" % product_id
+            query = "DELETE * FROM products WHERE id = '%s';" % product_id
             cur.execute(query)
         return False
 
-    @staticmethod 
+    @staticmethod
+    def get_by_id(product_id):
+        if product_id:
+            query = "SELECT * FROM products WHERE id = '%s';" % product_id
+            cur.execute(query)
+            return cur.fetchone()
+        return False
+
+    @staticmethod
     def update(payload, id):
         pass
-
-
-            
-
