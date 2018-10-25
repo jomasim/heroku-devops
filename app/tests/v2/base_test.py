c680f0167e36eb0c7b509773a81b75c760d2252e
Joma Simeon <simjoms@gmail.com>
2018-10-25 14:13:38 +0300

    [ft #161468072] refactored base test

--- app/tests/v2/base_test.py
+++ app/tests/v2/base_test.py
@@ -8,14 +8,42 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client(self)
         create_testdb()
+        ''' create sample login user in test db '''
+
+        create_user_url = "/api/v1/user"
+        auth_url = "/api/v1/user"
+
+        sample_user = {
+            'name': 'jane doe',
+            'email': 'janedoe@gmail.com',
+            'username': 'john',
+            'password':'123456'
+        }
+
+        response = self.client.post(create_user_url,
+                                    data=json.dumps(sample_user),
+                                    content_type='application/json')
+        if response.status_code == 201 :
+
+            ''' generate token from sample user if creation was successful '''
+
+            data= {"username": "joma", "password": "123456"}
+
+            response = self.client.post(auth_url,
+                                        data=json.dumps(data),
+                                        content_type='application/json')
+            data=json.loads(response.data)
+            self.token="Bearer " + data ['access_token']
+
 
     def get(self, url):
-        return self.client.get(url)
+        return self.client.get(url, headers={"Authorization": self.token})
 
     def post(self, url, data):
         return self.client.post(url,
                                 data=json.dumps(data),
-                                content_type='application/json')
+                                content_type='application/json', 
+                                headers={"Authorization": self.token})
 
     def tearDown(self):
         drop_testdb()
