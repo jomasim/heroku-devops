7cb63d4e30c6199329e0c98ca6dbbdcdf7fa1444
Joma Simeon <simjoms@gmail.com>
2018-10-28 12:40:46 +0300

    [ch #161527250] run database migration

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from app.schema.db_utils import create_db
 
 app=create_app('development')
 
 if __name__ == '__main__':
+
+    ''' migrate tables '''
+    create_db()
     app.run(debug=True)
