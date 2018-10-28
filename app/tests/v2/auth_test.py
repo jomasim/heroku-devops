a2101cf127280fe61d96a16be8f2dea6fac55619
Joma Simeon <simjoms@gmail.com>
2018-10-28 13:04:13 +0300

    [ch # 161527250] refactor login tests

--- app/tests/v2/auth_test.py
+++ app/tests/v2/auth_test.py
@@ -21,4 +21,28 @@ class TestAuth(BaseTestCase):
                 }
         response = self.post('/api/v2/login', data=user)
         self.assertTrue(response)
+        data=json.loads(response.data)
+        self.assertEqual({"message":data['message']}, {
+                         'message': 'login successful'})
         self.assertEqual(response.status_code,200)
+
+    def test_login_with_invalid_credentials(self):
+        user={"email": "kimdoe@gmail.com",
+            "password": "1234566343"
+                }
+        response = self.post('/api/v2/login', data=user)
+        self.assertTrue(response)
+        self.assertEqual(json.loads(response.data), {
+                         'message': 'invalid login'})
+        self.assertEqual(response.status_code,401)
+
+    def test_login_with_empty_data(self):
+        user={"email": "",
+            "password": ""
+                }
+        response = self.post('/api/v2/login', data=user)
+        self.assertTrue(response)
+        self.assertEqual(json.loads(response.data), 
+        {'errors': {'email': ['email is required'],
+        'password': ['password is required']}})
+        self.assertEqual(response.status_code,422)
