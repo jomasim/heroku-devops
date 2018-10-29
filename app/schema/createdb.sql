e3c8ab1b46ed875347d4fee5b9caca41a6865676
Joma Simeon <simjoms@gmail.com>
2018-10-29 10:02:07 +0300

    [ft #161538815] add sales table schema

--- app/schema/createdb.sql
+++ app/schema/createdb.sql
@@ -19,3 +19,11 @@ create table if not exists products(
     create_at timestamp not null default now(),
     updated_at timestamp default current_timestamp
 );
+
+create table if not exists sales(
+    id serial not null primary key,
+    line_items text not null,
+    created_by integer not null references users(id),
+    create_at timestamp not null default now(),
+    updated_at timestamp default current_timestamp
+);
